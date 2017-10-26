88959584a97b9be30a6729a21ee3bae93d6582ec
Milan Nikolic <gen2brain@gmail.com>
2017-10-26 19:39:38 +0200

    RPi - add default include paths

--- raylib/cgo_linux_arm.go
+++ raylib/cgo_linux_arm.go
@@ -4,6 +4,6 @@ package raylib
 
 /*
 #cgo linux,arm LDFLAGS: -lGLESv2 -lEGL -lpthread -lrt -lm -lbcm_host -lvcos -lvchiq_arm -lopenal
-#cgo linux,arm CFLAGS: -DPLATFORM_RPI -DGRAPHICS_API_OPENGL_ES2
+#cgo linux,arm CFLAGS: -DPLATFORM_RPI -DGRAPHICS_API_OPENGL_ES2 -I/opt/vc/include -I/opt/vc/include/interface/vcos -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads
 */
 import "C"
