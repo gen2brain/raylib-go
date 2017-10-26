f8227a3d71c277064752cf67cfe6a491ebec9354
Milan Nikolic <gen2brain@gmail.com>
2017-10-26 19:42:58 +0200

    RPi - use brcm libs

--- raylib/cgo_linux_arm.go
+++ raylib/cgo_linux_arm.go
@@ -3,7 +3,7 @@
 package raylib
 
 /*
-#cgo linux,arm LDFLAGS: -lGLESv2 -lEGL -lpthread -lrt -lm -lbcm_host -lvcos -lvchiq_arm -lopenal
+#cgo linux,arm LDFLAGS: -lbrcmGLESv2 -lbrcmEGL -lpthread -lrt -lm -lbcm_host -lvcos -lvchiq_arm -lopenal
 #cgo linux,arm CFLAGS: -DPLATFORM_RPI -DGRAPHICS_API_OPENGL_ES2 -I/opt/vc/include -I/opt/vc/include/interface/vcos -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads
 */
 import "C"
