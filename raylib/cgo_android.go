ea323e7051b6e1833c9f9194245854d02a69c689
Milan Nikolic <gen2brain@gmail.com>
2017-10-27 23:08:18 +0200

    Update Android

--- raylib/cgo_android.go
+++ raylib/cgo_android.go
@@ -3,7 +3,7 @@
 package raylib
 
 /*
-#cgo android LDFLAGS: -llog -landroid -lEGL -lGLESv2 -lOpenSLES -lm -landroid_native_app_glue
+#cgo android LDFLAGS: -llog -landroid -lEGL -lGLESv2 -lOpenSLES -lm -landroid_native_app_glue -u ANativeActivity_onCreate
 #cgo android CFLAGS: -DPLATFORM_ANDROID -DGRAPHICS_API_OPENGL_ES2
 
 #cgo android,!noaudio LDFLAGS: -lopenal
