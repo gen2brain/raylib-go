c7ce53dba726659f9512d24ea73eb93f90ab8f5a
Milan Nikolic <gen2brain@gmail.com>
2017-10-27 15:29:08 +0200

    LockOSThread

--- raylib/raylib.go
+++ raylib/raylib.go
@@ -1,2 +1,10 @@
 // Package raylib - Go bindings for raylib, a simple and easy-to-use library to learn videogames programming
 package raylib
+
+import (
+	"runtime"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
