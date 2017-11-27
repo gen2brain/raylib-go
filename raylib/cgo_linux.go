9825f0a9164362e81c407d292f7ab09594208830
Milan Nikolic <gen2brain@gmail.com>
2017-11-27 05:56:25 +0100

    Add Wayland support. Fixes #24

--- raylib/cgo_linux.go
+++ raylib/cgo_linux.go
@@ -10,10 +10,18 @@ package raylib
 #include "external/glfw/src/vulkan.c"
 #include "external/glfw/src/window.c"
 
+#ifdef _GLFW_WAYLAND
+#include "external/glfw/src/wl_init.c"
+#include "external/glfw/src/wl_monitor.c"
+#include "external/glfw/src/wl_window.c"
+#endif
+#ifdef _GLFW_X11
 #include "external/glfw/src/x11_init.c"
 #include "external/glfw/src/x11_monitor.c"
 #include "external/glfw/src/x11_window.c"
 #include "external/glfw/src/glx_context.c"
+#endif
+
 #include "external/glfw/src/linux_joystick.c"
 #include "external/glfw/src/posix_time.c"
 #include "external/glfw/src/posix_tls.c"
@@ -21,13 +29,15 @@ package raylib
 #include "external/glfw/src/egl_context.c"
 
 #cgo linux LDFLAGS: -lGL -lm -pthread -ldl -lrt -lX11 -lXrandr -lXinerama -lXi -lXxf86vm -lXcursor
-#cgo linux CFLAGS: -D_GLFW_X11 -Iexternal/glfw/include -DPLATFORM_DESKTOP
+#cgo linux CFLAGS: -Iexternal/glfw/include -DPLATFORM_DESKTOP
 
 #cgo linux,!noaudio LDFLAGS: -lopenal
 
 #cgo linux,!static CFLAGS: -DSHARED_OPENAL
 #cgo linux,static CFLAGS: -DAL_LIBTYPE_STATIC
 
+#cgo linux,!wayland CFLAGS: -D_GLFW_X11
+#cgo linux,wayland CFLAGS: -D_GLFW_WAYLAND
 
 #cgo linux,opengl11 CFLAGS: -DGRAPHICS_API_OPENGL_11
 #cgo linux,opengl21 CFLAGS: -DGRAPHICS_API_OPENGL_21
