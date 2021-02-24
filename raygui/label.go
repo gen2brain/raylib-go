a406f4bf0311cc3bb3a2bcf0f8d22cb163eca7c9
Oliver 'kfsone' Smith <oliver@kfs.org>
2021-02-24 13:13:07 -0800

    Label cleanup

--- raygui/label.go
+++ raygui/label.go
@@ -9,20 +9,19 @@ func Label(bounds rl.Rectangle, text string) {
 
 // LabelEx - Label element extended, configurable colors
 func LabelEx(bounds rl.Rectangle, text string, textColor, border, inner rl.Color) {
-	b := bounds.ToInt32()
 	// Update control
-	textHeight := int32(style[GlobalTextFontsize])
+	textHeight := GetStyle32(GlobalTextFontsize)
 	textWidth := rl.MeasureText(text, textHeight)
 
+	b := bounds.ToInt32()
 	if b.Width < textWidth {
-		b.Width = textWidth + int32(style[LabelTextPadding])
+		b.Width = textWidth + GetStyle32(LabelTextPadding)
 	}
 	if b.Height < textHeight {
-		b.Height = textHeight + int32(style[LabelTextPadding])/2
+		b.Height = textHeight + GetStyle32(LabelTextPadding)/2
 	}
 
 	// Draw control
-	rl.DrawRectangle(b.X, b.Y, b.Width, b.Height, border)
-	rl.DrawRectangle(b.X+int32(style[LabelBorderWidth]), b.Y+int32(style[LabelBorderWidth]), b.Width-(2*int32(style[LabelBorderWidth])), b.Height-(2*int32(style[LabelBorderWidth])), inner)
+	DrawBorderedRectangle(b, GetStyle32(LabelBorderWidth), border, inner)
 	rl.DrawText(text, b.X+((b.Width/2)-(textWidth/2)), b.Y+((b.Height/2)-(textHeight/2)), textHeight, textColor)
 }
