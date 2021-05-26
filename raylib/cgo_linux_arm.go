84efeb32cf6e0f42b47c2b0167f82531e997eeac
Milan Nikolic <gen2brain@gmail.com>
2021-05-26 12:03:02 +0200

    Remove arm files

--- raylib/cgo_linux_arm.go
+++
@@ -1,9 +0,0 @@
-// +build linux,arm,!android
-
-package rl
-
-/*
-#cgo linux,arm LDFLAGS: -L/opt/vc/lib -L/opt/vc/lib64 -lbrcmGLESv2 -lbrcmEGL -lpthread -lrt -lm -lbcm_host -lvcos -lvchiq_arm -ldl
-#cgo linux,arm CFLAGS: -DPLATFORM_RPI -DGRAPHICS_API_OPENGL_ES2 -I/opt/vc/include -I/opt/vc/include/interface/vcos -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads
-*/
-import "C"
