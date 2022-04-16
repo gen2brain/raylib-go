32324342245ac7bd20b1112e3d74fe9da69a6e19
Milan Nikolic <gen2brain@gmail.com>
2022-04-16 14:15:06 +0200

    Fix warning -Wdll-attribute-on-redeclaration

--- raylib/utils_log.h
+++ raylib/utils_log.h
@@ -4,7 +4,6 @@ extern "C" {            // Prevents name mangling of functions
 
 
 void setLogCallbackWrapper(void);                 // enable the call-back
-void internalTraceLogCallbackGo(int, void*, int); // Go function that will get called
 
 #if defined(__cplusplus)
 }
