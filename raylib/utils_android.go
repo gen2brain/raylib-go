bd025d2b1e113c1623fafc96d49bc7ce1be56d20
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2022-10-25 22:02:00 +0200

    Create utils_android.go

---
+++ raylib/utils_android.go
@@ -0,0 +1,10 @@
+//go:build android
+// +build android
+
+package rl
+
+// HomeDir - Returns user home directory
+// NOTE: On Android this returns internal data path and must be called after InitWindow
+func HomeDir() string {
+		return getInternalStoragePath()
+}
