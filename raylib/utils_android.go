e33a1830c9d530a3662b78d20325fb021947a2c6
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2022-10-25 22:09:18 +0200

    Update utils_android.go

--- raylib/utils_android.go
+++ raylib/utils_android.go
@@ -3,6 +3,35 @@
 
 package rl
 
+/*
+#include "stdlib.h"
+#include "raylib.h"
+void TraceLogWrapper(int logLevel, const char *text)
+{
+	TraceLog(logLevel, text);
+}
+*/
+import "C"
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+// Set the current threshold (minimum) log level
+func SetTraceLog(logLevel TraceLogLevel) {
+	clogLevel := (C.int)(logLevel)
+	C.SetTraceLogLevel(clogLevel)
+}
+
+// Show trace log messages (LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR...)
+func TraceLog(logLevel TraceLogLevel, text string, v ...interface{}) {
+	ctext := C.CString(fmt.Sprintf(text, v...))
+	defer C.free(unsafe.Pointer(ctext))
+	clogLevel := (C.int)(logLevel)
+	C.TraceLogWrapper(clogLevel, ctext)
+}
+
 // HomeDir - Returns user home directory
 // NOTE: On Android this returns internal data path and must be called after InitWindow
 func HomeDir() string {
