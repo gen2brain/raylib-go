2904dbeb86b2a7fe25f271e336b19b9678aee9a1
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2022-11-11 14:01:12 +0100

    Typo fixed in cgo_freebsd.go

--- raylib/cgo_freebsd.go
+++ raylib/cgo_freebsd.go
@@ -36,7 +36,7 @@ package rl
 #cgo freebsd CFLAGS: -I/usr/local/include -Iexternal/glfw/include -DPLATFORM_DESKTOP
 #cgo freebsd LDFLAGS: -L/usr/local/lib
 
-#cgo freensd,!wayland LDFLAGS: -lGL -lm -pthread -ldl -lrt -lX11
+#cgo freebsd,!wayland LDFLAGS: -lGL -lm -pthread -ldl -lrt -lX11
 #cgo freebsd,wayland LDFLAGS: -lGL -lm -pthread -ldl -lrt -lwayland-client -lwayland-cursor -lwayland-egl -lxkbcommon
 
 #cgo freebsd,!wayland CFLAGS: -D_GLFW_X11
