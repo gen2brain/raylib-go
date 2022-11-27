129deee5bece7e73c7a21a9f42c6031f9f9fc87b
Milan Nikolic <gen2brain@gmail.com>
2022-11-27 20:28:31 +0100

    Fmt

--- raylib/utils.go
+++ raylib/utils.go
@@ -35,7 +35,7 @@ func TraceLog(logLevel TraceLogLevel, text string, v ...interface{}) {
 
 // HomeDir - Returns user home directory
 // NOTE: On Android this returns internal data path and must be called after InitWindow
-func HomeDir() string {	
+func HomeDir() string {
 	if homeDir, err := os.UserHomeDir(); err == nil {
 		return homeDir
 	}
