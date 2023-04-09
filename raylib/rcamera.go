70e5708243b61c87458735695bcabf2c11b69061
JupiterRider <60042618+JupiterRider@users.noreply.github.com>
2023-04-09 11:16:17 +0200

    UpdateCameraPro function added in raylib/rcamera.go

--- raylib/rcamera.go
+++ raylib/rcamera.go
@@ -13,3 +13,9 @@ func UpdateCamera(camera *Camera, mode CameraMode) {
 	ccamera := camera.cptr()
 	C.UpdateCamera(ccamera, C.int(mode))
 }
+
+// UpdateCameraPro - Update camera movement/rotation
+func UpdateCameraPro(camera *Camera, movement Vector3, rotation Vector3, zoom float32) {
+	ccamera := camera.cptr()
+	C.UpdateCameraPro(ccamera, *movement.cptr(), *rotation.cptr(), C.float(zoom))
+}
