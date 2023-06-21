ebac46345c4f5381f50f891e70178ca535d68786
Milan Nikolic <gen2brain@gmail.com>
2023-06-21 13:05:08 +0200

    Update Wayland

--- raylib/external/scripts/glfw-generate-wayland.sh
+++ raylib/external/scripts/glfw-generate-wayland.sh
@@ -4,37 +4,48 @@ TMPDIR="/tmp"
 GLGLFW_PATH="`pwd`/../glfw/src"
 
 cd $TMPDIR
-git clone https://github.com/wayland-project/wayland-protocols
+git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland.git
+git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland-protocols.git
+
+cd $TMPDIR
+cd wayland
+
+rm -f "$GLGLFW_PATH"/wayland-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-client-protocol-code.h
+
+wayland-scanner private-code ./protocol/wayland.xml "$GLGLFW_PATH"/wayland-client-protocol-code.h
+wayland-scanner client-header ./protocol/wayland.xml "$GLGLFW_PATH"/wayland-client-protocol.h
+
+cd $TMPDIR
 cd wayland-protocols
 
-rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.{h,c}
-rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.{h,c}
-rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.{h,c}
-rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.{h,c}
-rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.{h,c}
-rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol.{h,c}
+rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol-code.h
+rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol-code.h
 
-wayland-scanner code ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.c
-wayland-scanner client-header ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.h
-
-wayland-scanner code ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.c
-wayland-scanner client-header ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.h
-
-wayland-scanner code ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.c
-wayland-scanner client-header ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.h
-
-wayland-scanner code ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.c
+wayland-scanner private-code ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol-code.h
 wayland-scanner client-header ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.h
 
-wayland-scanner code ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.c
+wayland-scanner private-code ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol-code.h
 wayland-scanner client-header ./unstable/xdg-decoration/xdg-decoration-unstable-v1.xml "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.h
 
-wayland-scanner code ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/wayland-viewporter-client-protocol.c
+wayland-scanner private-code ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/wayland-viewporter-client-protocol-code.h
 wayland-scanner client-header ./stable/viewporter/viewporter.xml "$GLGLFW_PATH"/wayland-viewporter-client-protocol.h
 
-# Patch for cgo
-sed -i "s|types|wl_pc_types|g" "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.c
-sed -i "s|types|wl_rp_types|g" "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.c
-sed -i "s|types|wl_ii_types|g" "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.c
-sed -i "s|types|wl_xs_types|g" "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.c
-sed -i "s|types|wl_v_types|g" "$GLGLFW_PATH"/wayland-viewporter-client-protocol.c
+wayland-scanner private-code ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/relative-pointer/relative-pointer-unstable-v1.xml "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.h
+
+wayland-scanner private-code ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/pointer-constraints/pointer-constraints-unstable-v1.xml "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.h
+
+wayland-scanner private-code ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol-code.h
+wayland-scanner client-header ./unstable/idle-inhibit/idle-inhibit-unstable-v1.xml "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.h
