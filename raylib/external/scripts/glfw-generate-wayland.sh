9def9b04c9ec9858224f934f341e17954f071a5e
Milan Nikolic <gen2brain@gmail.com>
2023-06-21 13:11:37 +0200

    Update Wayland

--- raylib/external/scripts/glfw-generate-wayland.sh
+++ raylib/external/scripts/glfw-generate-wayland.sh
@@ -10,7 +10,7 @@ git clone --depth 1 https://gitlab.freedesktop.org/wayland/wayland-protocols.git
 cd $TMPDIR
 cd wayland
 
-rm -f "$GLGLFW_PATH"/wayland-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-client-protocol-code.h
 
 wayland-scanner private-code ./protocol/wayland.xml "$GLGLFW_PATH"/wayland-client-protocol-code.h
@@ -19,17 +19,17 @@ wayland-scanner client-header ./protocol/wayland.xml "$GLGLFW_PATH"/wayland-clie
 cd $TMPDIR
 cd wayland-protocols
 
-rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-xdg-decoration-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-viewporter-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-relative-pointer-unstable-v1-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-pointer-constraints-unstable-v1-client-protocol-code.h
-rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.c
+rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol.h
 rm -f "$GLGLFW_PATH"/wayland-idle-inhibit-unstable-v1-client-protocol-code.h
 
 wayland-scanner private-code ./stable/xdg-shell/xdg-shell.xml "$GLGLFW_PATH"/wayland-xdg-shell-client-protocol-code.h
