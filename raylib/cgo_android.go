e8ff82b39424c183dd4b0c7d39677082a2a316a8
Milan Nikolic <gen2brain@gmail.com>
2023-11-04 14:47:09 +0100

    Fix Android build

--- raylib/cgo_android.go
+++ raylib/cgo_android.go
@@ -7,6 +7,9 @@ package rl
 #include "external/android/native_app_glue/android_native_app_glue.c"
 
 #cgo android LDFLAGS: -llog -landroid -lEGL -lGLESv2 -lOpenSLES -lm
-#cgo android CFLAGS: -DPLATFORM_ANDROID -DGRAPHICS_API_OPENGL_ES2 -Iexternal/android/native_app_glue
+#cgo android CFLAGS: -DPLATFORM_ANDROID -DGRAPHICS_API_OPENGL_ES2 -Iexternal/android/native_app_glue -Wno-implicit-const-int-float-conversion
+
+#cgo android,arm CFLAGS: -march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16
+#cgo android,arm64 CFLAGS: -target aarch64 -mfix-cortex-a53-835769
 */
 import "C"
