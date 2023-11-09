3c61a95ab0d967ee0725721766d1bf8a8c64fd5b
Milan Nikolic <gen2brain@gmail.com>
2023-11-09 09:44:48 +0100

    Update cgo

--- raylib/cgo.go
+++ raylib/cgo.go
@@ -1,6 +1,6 @@
 package rl
 
 /*
-#cgo CFLAGS: -std=gnu99 -Wno-missing-braces -Wno-unused-result -Wno-implicit-function-declaration -Wno-discarded-qualifiers
+#cgo CFLAGS: -std=gnu99 -Wno-missing-braces -Wno-unused-result -Wno-implicit-function-declaration
 */
 import "C"
