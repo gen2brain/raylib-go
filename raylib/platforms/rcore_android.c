71d00e0da7eecc6aa0a582c0d346c9eadc169b27
MatejMagat305 <61238240+MatejMagat305@users.noreply.github.com>
2024-03-28 22:34:52 +0100

    Update rcore_android.c

--- raylib/platforms/rcore_android.c
+++ raylib/platforms/rcore_android.c
@@ -50,6 +50,7 @@
 #include <android/window.h>             // Required for: AWINDOW_FLAG_FULLSCREEN definition and others
 //#include <android/sensor.h>           // Required for: Android sensors functions (accelerometer, gyroscope, light...)
 #include <jni.h>                        // Required for: JNIEnv and JavaVM [Used in OpenURL()]
+#include <android/native_activity.h>
 
 #include <EGL/egl.h>                    // Native platform windowing system interface
 
@@ -129,9 +130,14 @@ void android_main(struct android_app *app)
 }
 
 // NOTE: Add this to header (if apps really need it)
-extern struct android_app *GetAndroidApp(void)
+struct android_app *GetAndroidApp(void)
 {
     return platform.app;
+}
+
+extern struct ANativeActivity *GetANativeActivity(void)
+{
+    return platform.app->activity;
 }
 
 //----------------------------------------------------------------------------------
