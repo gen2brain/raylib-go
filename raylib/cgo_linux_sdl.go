7b368d3f3755ca10cbd92abe8cbbee84bc45d121
brett bronson <brett.bronson@gmail.com>
2025-12-18 17:59:14 -0800

    Adding fixes from https://github.com/raysan5/raylib/issues/5403

--- raylib/cgo_linux_sdl.go
+++ raylib/cgo_linux_sdl.go
@@ -5,7 +5,7 @@ package rl
 
 /*
 #cgo linux,!es2 LDFLAGS: -lm
-#cgo linux CFLAGS: -Wno-stringop-overflow -Dgbutton=cbutton -DSDL_GetJoysticks=rl_sdl2_GetJoysticks -include sdl_shim.h
+#cgo linux CFLAGS: -Wno-stringop-overflow
 #cgo linux,sdl CFLAGS: -DPLATFORM_DESKTOP_SDL -DUSING_SDL2_PROJECT
 #cgo linux,sdl pkg-config: sdl2
 
@@ -17,23 +17,5 @@ package rl
 #cgo linux,!opengl11,!opengl21,!opengl43,!es2,!es3 CFLAGS: -DGRAPHICS_API_OPENGL_33
 #cgo linux,es2,!es3 CFLAGS: -DGRAPHICS_API_OPENGL_ES2
 #cgo linux,es3,!es2 CFLAGS: -DGRAPHICS_API_OPENGL_ES3
-
-#ifdef USING_SDL2_PROJECT
-#include "SDL2/SDL.h"
-SDL_JoystickID* rl_sdl2_GetJoysticks(int* count) {
-    int n = SDL_NumJoysticks();
-    if (count) *count = n;
-    SDL_JoystickID* ids = (SDL_JoystickID*)SDL_malloc(sizeof(SDL_JoystickID) * (n > 0 ? n : 1));
-    if (!ids) return NULL;
-    for (int i = 0; i < n; ++i) {
-        if (!SDL_IsGameController(i)) { ids[i] = -1; continue; }
-        SDL_GameController* gc = SDL_GameControllerOpen(i);
-        if (!gc) { ids[i] = -1; continue; }
-        ids[i] = SDL_JoystickInstanceID(SDL_GameControllerGetJoystick(gc));
-        SDL_GameControllerClose(gc);
-    }
-    return ids;
-}
-#endif
 */
 import "C"
